d39b1b42292232e33a784c29bd9ab90b6bd6fcf8
pengxiaofeng <qhpbdt66@gmail.com>
2016-12-21 17:24:41 +0800

    replace root with groups#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   resources :groups
 
-  root 'welcome#index'
+
+  root 'groups#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
